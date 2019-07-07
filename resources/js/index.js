bc01c1be6125e652830b915b819b53d7a0938222
Gina2015 <georginasheehan@gmail.com>
2019-07-07 22:23:44 +0100

    Added in random color for the shapes

--- resources/js/index.js
+++ resources/js/index.js
@@ -13,7 +13,9 @@ var timeOut;
 var shape = document.getElementById("shape");
 var maxShapeWidth = document.getElementById('shape').offsetWidth;
 var maxShapeHeight = document.getElementById('shape').offsetHeight;
-
+var shapeColors = ["blue", "red", "green", "yellow", "white", "lime", "gold", "violet", "aqua", "black", "blueviolet"];
+var previousColor = "red";
+var currentColor = previousColor;
 
 // Time taken to click
 function addTimeTaken(time) {
@@ -31,7 +33,6 @@ function createShape() {
 
 function randomizeShapeAppearance() {
   var shapeWidth = Math.floor((Math.random() * maxShapeWidth));
-  var shapeHeight = shapeWidth;
 
   // If shape width is too small (under 40px) then recalculate until bigger than 40px
   while (shapeWidth < 40)
@@ -45,7 +46,7 @@ function randomizeShapeAppearance() {
   var maxXPosition = document.getElementById('main').offsetWidth - shapeWidth;
 
   // Base Y Position of shape in div based on Div Height and height of shape
-  var maxYPosition = document.getElementById('main').offsetHeight - shapeHeight;
+  var maxYPosition = document.getElementById('main').offsetHeight - shapeWidth;
 
   console.log("maxXPosition : ", maxXPosition);
   console.log("maxYPosition : ", maxYPosition);
@@ -54,11 +55,19 @@ function randomizeShapeAppearance() {
   var yPosition = getRandomXYPosition(maxYPosition);
   var xPosition = getRandomXYPosition(maxYPosition);
 
-  // Set Styles for the shape
+  // Set Positions for the shape
   shape.style.top = yPosition + 'px';
   shape.style.left = xPosition + 'px';
   shape.style.width = shapeWidth + 'px';
-  shape.style.height = shapeHeight + 'px';
+  shape.style.height = shapeWidth + 'px';
+
+  // Get Random Color from array for shapeWidth
+  var color = getRandomColor();
+
+  console.log("Color is: ", color);
+  //var colorString = color.toString();
+  //console.log("colorString : ", colorString);
+  shape.style.backgroundColor = color;
 }
 
 // onClick Function
@@ -80,4 +89,23 @@ function getRandomXYPosition(seed) {
   var x = Math.floor((Math.random() * seed) + 1); // - ($('main').width() - $('shape').width());
   console.log(x);
   return x;
+}
+
+function getRandomColor() {
+  var random = Math.floor((Math.random() * 10));
+  console.log("Random Number is : ", random);
+
+  var color = shapeColors[random];
+  console.log("random color from array is : ", color);
+
+  while(color == previousColor)
+  {
+    var random = Math.floor((Math.random() * 10));
+    var color = shapeColors[random];
+  }
+
+  currentColor = color;
+  previousColor = color;
+
+  return color;
 }
